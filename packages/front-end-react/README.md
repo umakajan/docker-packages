e723a6e00e4df8169387762e09e35e1e82a3996a
Umakajan Umakanthan <umakajan@umakajan.com>
2020-02-05 17:44:26 -0500

    add front-end react package

---
+++ packages/front-end-react/README.md
@@ -0,0 +1,1 @@
+# packages/front-end-react
---
+++ packages/front-end-react/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => {
+  return <h1>Docker React Package</h1>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
